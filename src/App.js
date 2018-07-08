40a2364f5e39b63731d0620a7329631e3d510b13
Shreyas Gaonkar <sggaonk@amazon.com>
2018-07-08 12:57:45 -0500

    minor updates

--- src/App.js
+++ src/App.js
@@ -2,19 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let defaultTextColor = '#5abbc9'
+
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+class Aggregate extends Component{
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+      <h2 style={defaultStyle}>Number Text</h2>
+    </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img />
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    let name = 'Shreyas'
-    let headerStyle = {color: 'red', 'font-size': '50px'}
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 style={headerStyle} className="App-title">Hello, {name} </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }
